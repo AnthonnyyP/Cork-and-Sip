d56c6b50aead431c39c892210e73134d9966b9a9
Micole Atkins <micoleatkins@gmail.com>
2023-06-16 12:46:46 -0500

    html for templates

--- corkandsip/main_app/models.py
+++ corkandsip/main_app/models.py
@@ -1,18 +1,23 @@
 from django.db import models
 
 # Create your models here.
+
+
 class WineTasting(models.Model):
-  name = models.CharField(max_length = 30)
-  date = models.CharField(max_length = 30)
-  time = models.DateTimeField()
+    name = models.CharField(max_length=30)
+    date = models.CharField(max_length=30)
+    time = models.DateTimeField()
+
 
 class Guest(models.Model):
-  user_name = models.Charfield(max_length = 30)
-  user_email = models.EmailField(max_length= 70, blank = True , unique = True)
-  user_phone = models.Charfield(max_length = 10)
+    user_name = models.Charfield(max_length=30)
+    user_email = models.EmailField(max_length=70, blank=True, unique=True)
+    user_phone = models.Charfield(max_length=10)
+
 
 class Collection(models.Model):
-  wine_name = models.CharField(max_length = 50)
+    wine_name = models.CharField(max_length=50)
+
 
 class Wine(models.Model):
-  wine_name = models.CharField(max_length = 50)
+    wine_name = models.CharField(max_length=50)
