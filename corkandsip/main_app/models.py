d1e74a1faeed0fa2ed9c7dda190201d71fcbc692
Tony <tnp087@gmail.com>
2023-06-16 14:18:06 -0400

    updated models

--- corkandsip/main_app/models.py
+++ corkandsip/main_app/models.py
@@ -4,22 +4,31 @@ from django.db import models
 
 
 class WineTasting(models.Model):
-    name = models.CharField(max_length=30)
-    date = models.CharField(max_length=30)
-    time = models.DateTimeField()
+  name = models.CharField(max_length=30)
+  date = models.CharField(max_length=30)
+  time = models.DateTimeField()
 
 
 class Guest(models.Model):
-    user_name = models.Charfield(max_length=30)
-    user_email = models.EmailField(max_length=70, blank=True, unique=True)
-    user_phone = models.Charfield(max_length=10)
+  user_name = models.Charfield(max_length=30)
+  user_email = models.EmailField(max_length=70, blank=True, unique=True)
+  user_phone = models.Charfield(max_length=10)
+
+  def __str__(self):
+    return self.user_name
 
 
 class Collection(models.Model):
-    wine_name = models.CharField(max_length=50)
+  wine_name = models.CharField(max_length=50)
+
+  def __str__(self):
+    return self.wine_name
 
 
 class Wine(models.Model):
   wine_name = models.CharField(max_length = 50)
   wine_age = models.CharField(max_length = 50)
   wine_origin = models.CharField(max_length = 50) 
+
+  def __str__(self):
+    return self.wine_name
