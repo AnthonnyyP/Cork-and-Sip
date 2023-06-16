effd17e48ac272ff34b9f4e41027c29ac8480027
James Aubrey <jaaubrey92@gmail.com>
2023-06-16 12:31:05 -0500

    added first three views

--- corkandsip/main_app/views.py
+++ corkandsip/main_app/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def home(request):
+    return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def collectors_index(request):
+    return render(request, 'collectors/index.html, {
+        'collectors': collectors
+    })
