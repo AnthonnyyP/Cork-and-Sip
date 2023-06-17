9dfa03d227f53a87d80a717e482641445321a63a
James Aubrey <jaaubrey92@gmail.com>
2023-06-17 15:16:07 -0500

    guest CRUD implemented

--- corkandsip/main_app/urls.py
+++ corkandsip/main_app/urls.py
@@ -6,5 +6,10 @@ urlpatterns = [
     path('about/', views.about, name='about'),
     path('guests/', views.guests_index, name='index'),
     path('guests/<int:guest_id>/', views.guests_detail, name='detail'),
+    path('guests/create/', views.GuestCreate.as_view(), name='guests_create'),
+    path('guests/<int:pk>/update/',
+         views.GuestUpdate.as_view(), name='guests_update'),
+    path('guests/<int:pk>/delete/',
+         views.GuestDelete.as_view(), name='guests_delete'),
     path('accounts/signup/', views.signup, name='signup'),
 ]
--- corkandsip/main_app/views.py
+++ corkandsip/main_app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Guest
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
@@ -42,3 +43,22 @@ def signup(request):
     form = UserCreationForm()
     context = {'form': form, 'error_message': error_message}
     return render(request, 'registration/signup.html', context)
+
+
+class GuestCreate(LoginRequiredMixin, CreateView):
+    model = Guest
+    fields = ['user_name', 'user_email', 'user_phone']
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
+
+
+class GuestUpdate(LoginRequiredMixin, UpdateView):
+    model = Guest
+    fields = ['user_email', 'user_phone']
+
+
+class GuestDelete(LoginRequiredMixin, DeleteView):
+    model = Guest
+    success_url = '/guests'
