d12bc89567451b928136c5b9dbe057b1e4fee090
James Aubrey <jaaubrey92@gmail.com>
2023-06-17 19:44:32 -0500

    fixed footer bar and guest detail page

--- corkandsip/main_app/views.py
+++ corkandsip/main_app/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render, redirect
-#CRUD Views
+# CRUD Views
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic import ListView, DetailView
 from .models import Guest
-#User Creation & Sign Up
+# User Creation & Sign Up
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
@@ -26,19 +26,21 @@ def about(request):
 def guests_index(request):
     guests = Guest.objects.filter(user=request.user)
     return render(request, 'guests/index.html',
-    {'guests': guests})
+                  {'guests': guests})
+
 
 @login_required
 def guests_detail(request, guest_id):
     guest = Guest.objects.get(id=guest_id)
-    id_list = guest.wines.all().values_list('id')
-    wines_guest_doesnt_have = Wine.objects.exclude(id__in = id_list)
+    id_list = guest.wine.all().values_list('id')
+    wines_guest_doesnt_have = Wine.objects.exclude(id__in=id_list)
     tasting_form = TastingForm()
     return render(request, 'guests/detail.html', {
-        'guest': guest , 
-        'feeding_form': feeding_form,
+        'guest': guest,
+        'tasting_form': tasting_form,
         'wines': wines_guest_doesnt_have
     })
+
 
 def signup(request):
     error_message = ''
@@ -73,35 +75,47 @@ class GuestDelete(LoginRequiredMixin, DeleteView):
     model = Guest
     success_url = '/guests'
 
+
 @login_required
 def add_tasting(request, guest_id):
     form = TastingForm(request.POST)
     if form.is_valid():
-        new_tasting = form.save(commit = False)
+        new_tasting = form.save(commit=False)
         new_tasting.guest_id = guest_id
         new_tasting.save()
-    return redirect('detail', guest_id = guest_id)
+    return redirect('detail', guest_id=guest_id)
+
 
 class WineList(LoginRequiredMixin, ListView):
     model = Wine
+
+
 class WineDetail(LoginRequiredMixin, DetailView):
     model = Wine
+
+
 class WineCreate(LoginRequiredMixin, CreateView):
     model = Wine
     fields = '__all__'
+
+
 class WineUpdate(LoginRequiredMixin, UpdateView):
     model = Wine
     fields = '__all__'
+
+
 class WineDelete(LoginRequiredMixin, DeleteView):
     model = Wine
     success_url = '/wines'
 
+
 @login_required
 def add_to_cellar(request, guest_id, wine_id):
-    Guest.objects.get(id = guest_id).wines.add(wine_id)
-    return redirect('detail', guest_id = guest_id)
+    Guest.objects.get(id=guest_id).wine.add(wine_id)
+    return redirect('detail', guest_id=guest_id)
+
 
 @login_required
 def remove_from_cellar(request, guest_id, wine_id):
-    Guest.objects.get(id = guest_id).wines.remove(wine_id)
-    return redirect('detail', guest_id = guest_id)
+    Guest.objects.get(id=guest_id).wine.remove(wine_id)
+    return redirect('detail', guest_id=guest_id)
